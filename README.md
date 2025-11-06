181a76ef6a40dfa9b517428cfc88e7f93805f90b
hqzing <hqzing@outlook.com>
2025-11-06 11:07:48 +0800

    第一个版本

--- README.md
+++ README.md
@@ -1,1 +1,116 @@
 # ohos-neovim
+
+本项目为 OpenHarmony 平台编译了 neovim，并发布预构建包。
+
+我们仅编译了 neovim 软件本身，没有提供插件方面的解决方案，有插件需求的用户需要自己动手解决。
+
+## 获取软件包
+
+前往 [release 页面](https://github.com/Harmonybrew/ohos-neovim/releases) 获取。
+
+## 用法
+**1\. 在鸿蒙 PC 中使用**
+
+鸿蒙 PC 内置了 vim，已经可以满足一般的文本编辑需求，建议直接使用系统自带的 vim。
+
+如果你仍希望使用 neovim，那也是可以的，只是操作比较复杂。
+
+因系统限制，我们无法以“解压 + 配 PATH” 的方式使用这个 neovim。你需要把它做成 hnp 包装入系统中，然后才能在“终端”（HiShell）中调用。详情请参考 [Termony
+](https://github.com/TermonyHQ/Termony) 的方案。
+
+**2\. 在鸿蒙开发板中使用**
+
+用 hdc 把它推到设备上，然后以“解压 + 配 PATH” 的方式使用。
+
+示例：
+```sh
+hdc file send neovim-0.11.4-ohos-arm64.tar.gz /data
+hdc shell
+
+cd /data
+tar -zxf neovim-0.11.4-ohos-arm64.tar.gz
+export PATH=$PATH:/data/neovim-0.11.4-ohos-arm64/bin
+export HOME=/data
+export TERM=screen-256color
+export TERMINFO=/data/neovim-0.11.4-ohos-arm64/share/terminfo
+
+# 现在可以使用 nvim 命令了
+```
+
+这个 neovim 在不同的上位机终端环境中都是可用的，包括 Cmd、PowerShell、Windows Terminal。
+
+只是需要注意，HOME、TERM、TERMINFO 这几个变量缺一不可，因为它们各自处理了不同的问题。
+
+尤其 TERM 变量的值，是有约束的。当前已知可用的 TERM 值有这些：screen, screen-256color, tmux, tmux-256color。
+
+存在这个约束，是因为 hdc 在连接 OpenHarmony 设备时会创建一个伪终端（pseudo-terminal），它的行为并不完全像一个标准的 xterm 或 xterm-256color 终端，而更接近于 screen 或 tmux 这类多路复用器的终端模拟方式。当我们设置成 screen 或 tmux 家族的值之后，hdc 发送的键码和 terminfo 定义的键码就能匹配了，就不会出现按键错位。
+
+**3\. 在 [鸿蒙容器](https://github.com/hqzing/docker-mini-openharmony) 中使用**
+
+在容器中用 curl 下载这个软件包，然后以“解压 + 配 PATH” 的方式使用。
+
+示例：
+```sh
+docker run -itd --name=ohos ghcr.io/hqzing/docker-mini-openharmony:latest
+docker exec -it ohos sh
+
+cd /root
+curl -L -O https://github.com/Harmonybrew/ohos-neovim/releases/download/0.11.4/neovim-0.11.4-ohos-arm64.tar.gz
+tar -zxf neovim-0.11.4-ohos-arm64.tar.gz -C /opt
+export PATH=/opt/neovim-0.11.4-ohos-arm64/bin
+
+# 现在可以使用 nvim 命令了
+```
+
+一般情况下，在容器中不需要额外设置环境变量就能正常使用这个 neovim 。如果你仍遇到了问题，请看下一个章节“常见问题”。
+
+## 从源码构建
+
+**1\. 手动构建**
+
+这个项目使用本地编译（native compilation，也可以叫本机编译或原生编译）的做法来编译鸿蒙版 neovim，而不是交叉编译。
+
+需要在鸿蒙容器中运行项目里的 build.sh，以实现 neovim 的本地编译。
+
+示例：
+```sh
+git clone https://github.com/Harmonybrew/ohos-neovim.git
+
+docker run -itd --name=ohos ghcr.io/hqzing/docker-mini-openharmony:latest
+docker cp ohos-neovim ohos:/root
+docker exec -it ohos sh
+
+cd /root/ohos-neovim
+./build.sh
+
+# 构建完成后会在容器中生成 /opt/neovim-0.11.4-ohos-arm64.tar.gz
+```
+
+**2\. 使用流水线构建**
+
+如果你熟悉 GitHub Actions，你可以直接复用项目内的工作流配置，使用 GitHub 的流水线来完成构建。
+
+这种情况下，你使用的是 GitHub 提供的构建机，不需要自己准备构建环境。
+
+只需要这么做，你就可以进行你的个人构建：
+1. Fork 本项目，生成个人仓
+2. 在个人仓的“Actions”菜单里面启用工作流
+3. 在个人仓提交代码或发版本，触发流水线运行
+
+## 常见问题
+
+**1\. 键盘按键异常**
+
+如果这个 neovim 在你的环境上出现了键盘按键异常的情况，可以尝试先设置 TERM 和 TERMINFO 环境变量，再启动它。
+
+```sh
+export TERM=screen-256color
+export TERMINFO=<neovim安装目录的绝对路径>/share/terminfo
+nvim
+```
+
+为了让用户能应对各种复杂的终端场景，我们在软件包里面里面放置了一个 `share/terminfo` 目录，里面包含了一个完整的 terminfo 数据库。
+
+因此，你只要将 TERMINFO 环境变量填成我们带进来的这个`share/terminfo` 目录，你就可以让 neovim 以及其他使用 terminfo 的程序识别这个 terminfo 数据库。
+
+在这基础上，设置一个与你终端环境匹配的 TERM 值，这应该可以解决你的绝大多数问题。如果还不能解决，可以在 issue 里面发起讨论。
---
+++ build.sh
@@ -0,0 +1,190 @@
+#!/bin/sh
+set -e
+
+alpine_repository="http://dl-cdn.alpinelinux.org/alpine/v3.22/main/aarch64/"
+
+download_alpine_index() {
+    curl -fsSL ${alpine_repository}/APKINDEX.tar.gz | tar -zx -C /tmp
+}
+
+get_apk_url() {
+    package_name=$1
+    package_version=$(grep -A1 "^P:${package_name}$" /tmp/APKINDEX | sed -n "s/^V://p")
+    apk_file_name=${package_name}-${package_version}.apk
+    echo ${alpine_repository}/${apk_file_name}
+}
+
+# 准备一些杂项的命令行工具
+download_alpine_index
+curl -L -O $(get_apk_url busybox-static)
+curl -L -O $(get_apk_url make)
+curl -L -O $(get_apk_url grep)
+curl -L -O $(get_apk_url pcre2)
+for file in *.apk; do
+  tar -zxf $file -C /
+done
+rm -rf *.apk
+rm /bin/xargs
+ln -s /bin/busybox.static /bin/xargs
+ln -s /bin/busybox.static /bin/tr
+ln -s /bin/busybox.static /bin/expr
+ln -s /bin/busybox.static /bin/awk
+ln -s /bin/busybox.static /bin/unzip
+
+# 准备 ohos-sdk
+# OpenHarmony 发布页（https://gitcode.com/openharmony/docs/blob/master/zh-cn/release-notes/OpenHarmony-v6.0-release.md）里面并没有发布鸿蒙版的 ohos-sdk，只发布了 Windows、Linux、Mac 版本
+# 为了进行本地编译，这里只能从 OpenHarmony 官方社区的每日构建流水线（https://ci.openharmony.cn/workbench/cicd/dailybuild/dailylist）下载 OpenHarmony 主干版本编出来的鸿蒙版 ohos-sdk
+sdk_ohos_download_url="https://cidownload.openharmony.cn/version/Master_Version/ohos-sdk-public_ohos/20251027_020623/version-Master_Version-ohos-sdk-public_ohos-20251027_020623-ohos-sdk-public_ohos.tar.gz"
+curl $sdk_ohos_download_url -o ohos-sdk-public_ohos.tar.gz
+mkdir /opt/ohos-sdk
+tar -zxf ohos-sdk-public_ohos.tar.gz -C /opt/ohos-sdk
+cd /opt/ohos-sdk/ohos/
+unzip -q native-*.zip
+unzip -q toolchains-*.zip
+cd - >/dev/null
+
+# 设置环境变量
+export PATH=$PATH:/opt/ohos-sdk/ohos/native/llvm/bin                # 编译器在这里面
+export PATH=$PATH:/opt/ohos-sdk/ohos/native/build-tools/cmake/bin   # cmake 和 ninja 在这里面
+export PATH=$PATH:/opt/ohos-sdk/ohos/toolchains/lib                 # 代码签名工具在这里面
+export CC=clang                                                     # 编 gettext、conv、ncurses 需要这几个变量
+export CXX=clang++
+export AR=llvm-ar
+export LD=ld.lld
+
+# 编译 gettext。要有这个库才能正常编出 neovim。
+curl -L -O http://mirrors.ustc.edu.cn/gnu/gettext/gettext-0.22.tar.gz
+tar -zxf gettext-0.22.tar.gz
+cd gettext-0.22
+./configure --prefix=/opt/gettext-0.22-ohos-arm64 --host=aarch64-linux --disable-shared 
+make -j$(nproc)
+make install
+cd ..
+
+# 编译 libiconv。要有这个库才能正常编出 neovim。
+curl -L -O http://mirrors.ustc.edu.cn/gnu/libiconv/libiconv-1.17.tar.gz
+tar -zxf libiconv-1.17.tar.gz
+cd libiconv-1.17
+./configure --prefix=/opt/libiconv-1.17-ohos-arm64 --host=aarch64-linux  --disable-shared
+make -j$(nproc)
+make install
+cd ..
+
+# 准备 neovim 源码
+curl -L https://github.com/neovim/neovim/archive/refs/tags/v0.11.4.tar.gz -o neovim-0.11.4.tar.gz
+tar -zxf neovim-0.11.4.tar.gz
+cd neovim-0.11.4
+
+# 将 neovim 依赖的 libuv 版本改成最新版（1.50.0 改成 1.51.0）
+# libuv 1.51.0 做了鸿蒙适配，解决了 pthread_getaffinity_np 接口不存在的编译报错
+sed -i 's|https://github.com/libuv/libuv/archive/v1.50.0.tar.gz|https://github.com/libuv/libuv/archive/v1.51.0.tar.gz|g' cmake.deps/deps.txt
+sed -i 's|b1ec56444ee3f1e10c8bd3eed16ba47016ed0b94fe42137435aaf2e0bd574579|27e55cf7083913bfb6826ca78cde9de7647cded648d35f24163f2d31bb9f51cd|g' cmake.deps/deps.txt
+
+# 由于 ohos-sdk 里面的 cmake 不支持下载 https 链接，所以这里先手动把依赖的文件下载好
+# cmake 检测到文件存在就不会去实时下载，这样可以避免报错
+curl --create-dirs -L https://github.com/luajit/luajit/archive/538a82133ad6fddfd0ca64de167c4aca3bc1a2da.tar.gz -o .deps/build/downloads/luajit/538a82133ad6fddfd0ca64de167c4aca3bc1a2da.tar.gz
+curl --create-dirs -L https://github.com/libuv/libuv/archive/v1.51.0.tar.gz -o .deps/build/downloads/libuv/v1.51.0.tar.gz
+curl --create-dirs -L https://github.com/neovim/unibilium/archive/v2.1.2.tar.gz -o .deps/build/downloads/unibilium/v2.1.2.tar.gz
+curl --create-dirs -L https://github.com/lunarmodules/lua-compat-5.3/archive/v0.13.tar.gz -o .deps/build/downloads/lua_compat53/v0.13.tar.gz
+curl --create-dirs -L https://github.com/tree-sitter/tree-sitter-c/archive/v0.24.1.tar.gz -o .deps/build/downloads/treesitter_c/v0.24.1.tar.gz
+curl --create-dirs -L https://github.com/neovim/tree-sitter-vimdoc/archive/v4.0.0.tar.gz -o .deps/build/downloads/treesitter_vimdoc/v4.0.0.tar.gz
+curl --create-dirs -L https://github.com/tree-sitter-grammars/tree-sitter-lua/archive/v0.4.0.tar.gz -o .deps/build/downloads/treesitter_lua/v0.4.0.tar.gz
+curl --create-dirs -L https://github.com/tree-sitter-grammars/tree-sitter-markdown/archive/v0.5.0.tar.gz -o .deps/build/downloads/treesitter_markdown/v0.5.0.tar.gz
+curl --create-dirs -L https://github.com/tree-sitter-grammars/tree-sitter-vim/archive/v0.7.0.tar.gz -o .deps/build/downloads/treesitter_vim/v0.7.0.tar.gz
+curl --create-dirs -L https://github.com/tree-sitter/tree-sitter/archive/v0.25.6.tar.gz -o .deps/build/downloads/treesitter/v0.25.6.tar.gz
+curl --create-dirs -L https://github.com/JuliaStrings/utf8proc/archive/v2.10.0.tar.gz -o .deps/build/downloads/utf8proc/v2.10.0.tar.gz
+curl --create-dirs -L https://github.com/tree-sitter-grammars/tree-sitter-query/archive/v0.6.2.tar.gz -o .deps/build/downloads/treesitter_query/v0.6.2.tar.gz
+curl --create-dirs -L https://github.com/luvit/luv/archive/1.50.0-1.tar.gz -o .deps/build/downloads/luv/1.50.0-1.tar.gz
+curl --create-dirs -L https://github.com/neovim/deps/raw/d495ee6f79e7962a53ad79670cb92488abe0b9b4/opt/lpeg-1.1.0.tar.gz -o .deps/build/downloads/lpeg/lpeg-1.1.0.tar.gz
+
+# 把 ar 命令创建出来，避免编到 luajit 的时候报错
+ln -s llvm-ar /opt/ohos-sdk/ohos/native/llvm/bin/ar
+
+# 编译 neovim 的捆绑依赖项
+cmake -S cmake.deps -B .deps -G Ninja \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D CMAKE_C_COMPILER=clang \
+  -D CMAKE_AR=llvm-ar \
+  -D CMAKE_LINKER=ld.lld \
+  -D CMAKE_SYSTEM_NAME=Linux \
+  -D CMAKE_SYSTEM_PROCESSOR=aarch64
+ninja -C .deps
+
+# 编译 neovim 本体
+cmake -B build -G Ninja \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D CMAKE_C_COMPILER=clang \
+  -D CMAKE_AR=llvm-ar \
+  -D CMAKE_LINKER=ld.lld \
+  -D CMAKE_SYSTEM_NAME=Linux \
+  -D CMAKE_SYSTEM_PROCESSOR=aarch64 \
+  -D LIBINTL_INCLUDE_DIR=/opt/gettext-0.22-ohos-arm64/include \
+  -D LIBINTL_LIBRARY=/opt/gettext-0.22-ohos-arm64/lib/libintl.a \
+  -D ICONV_INCLUDE_DIR=/opt/libiconv-1.17-ohos-arm64/include \
+  -D ICONV_LIBRARY=/opt/libiconv-1.17-ohos-arm64/lib/libiconv.a \
+  -D BUILD_SHARED_LIBS=OFF \
+  -D CMAKE_INSTALL_PREFIX=/opt/neovim-0.11.4-ohos-arm64
+ninja -C build install
+cd ..
+
+# 编译 ncurses，生成 terminfo 数据库，并把 terminfo 数据库复制到 neovim 的安装目录中
+# 并不是所有 OpenHarmony 环境上都有 terminfo 数据库，为了让 neovim 在尽可能多的环境中可用，这里需要放一份 terminfo 数据库到 neovim 的安装目录中，随 neovim 一同发布
+curl -L -O https://mirrors.ustc.edu.cn/gnu/ncurses/ncurses-6.5.tar.gz
+tar -zxf ncurses-6.5.tar.gz
+cd ncurses-6.5
+./configure \
+  --host=aarch64-linux \
+  --prefix=/opt/ncurses-6.5-ohos-arm64 \
+  --enable-database
+make -j$(nproc)
+make install
+cd ..
+cp -r /opt/ncurses-6.5-ohos-arm64/share/terminfo /opt/neovim-0.11.4-ohos-arm64/share/terminfo
+
+# 履行开源义务，把使用的开源软件的 license 全部聚合起来放到制品中
+neovim_license=$(cat neovim-0.11.4/LICENSE.txt; echo)
+gettext_license=$(cat gettext-0.22/COPYING; echo)
+gettext_authors=$(cat gettext-0.22/AUTHORS; echo)
+libiconv_license=$(cat libiconv-1.17/COPYING; echo)
+libiconv_authors=$(cat libiconv-1.17/AUTHORS; echo)
+ncurses_license=$(cat ncurses-6.5/COPYING; echo)
+ncurses_authors=$(cat ncurses-6.5/AUTHORS; echo)
+printf '%s' "$(cat <<EOF
+This document describes the licenses of all software distributed with the
+bundled application.
+==========================================================================
+
+neovim
+=============
+$neovim_license
+
+gettext
+=============
+==license==
+$gettext_license
+==authors==
+$gettext_authors
+
+libiconv
+=============
+==license==
+$libiconv_license
+==authors==
+$libiconv_authors
+
+ncurses
+=============
+==license==
+$ncurses_license
+==authors==
+$ncurses_authors
+EOF
+)" > /opt/neovim-0.11.4-ohos-arm64/licenses.txt
+
+# 代码签名。做这一步是为了现在或以后能让它运行在 OpenHarmony 的商业发行版——HarmonyOS 上。
+binary-sign-tool sign -inFile /opt/neovim-0.11.4-ohos-arm64/bin/nvim -outFile /opt/neovim-0.11.4-ohos-arm64/bin/nvim -selfSign 1
+find /opt/neovim-0.11.4-ohos-arm64/lib/nvim/parser -type f | xargs -I {} binary-sign-tool sign -inFile {} -outFile {} -selfSign 1 
+
+# 打包最终产物
+cd /opt
+tar -zcf neovim-0.11.4-ohos-arm64.tar.gz neovim-0.11.4-ohos-arm64
